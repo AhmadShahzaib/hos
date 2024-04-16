f272027737b20f15e01e77acb5d930a35c2fb709
Ahmad Shahzaib <ahmadshahzaib-tekhqs>
2024-04-16 12:36:36 +0500

    new

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,11 @@ import * as requestIp from 'request-ip';
 import { json } from 'express';
 import { CustomInterceptor } from 'utils/customInterceptor';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders:"*",
-    origin: "*"
-});
+  const app = await NestFactory.create(AppModule, { cors: true });
+//   app.enableCors({
+//     allowedHeaders:"*",
+//     origin: "*"
+// });
   app.useWebSocketAdapter(new IoAdapter(app));
   const logger = new Logger('Main');
   const globalPrefix = '/api';
